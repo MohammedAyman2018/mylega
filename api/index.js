582fca9d891e2388850223ee838485cc57958064
MohammedAyman2018 <emidobibo20@gmail.com>
2021-07-27 06:10:22 +0200

    feat: Add cors.

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
-// const cors = require('cors')
+const cors = require('cors')
 
 // Create express instance
 const app = express()
-// const corsOptions = {
-//   origin: '*',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+const corsOptions = {
+  origin: 'https://league-two.vercel.app/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 /** Connect to db */
 async function db () {
   // process.env.DB_URL
-  // mongodb://localhost:27017/test
+  // mongodb://localhost:27017/league
   await mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -23,7 +23,7 @@ async function db () {
 };
 db()
 
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
